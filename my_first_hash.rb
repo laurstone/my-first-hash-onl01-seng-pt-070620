0f5064c622015a19ceba9d7bfabff09fa5a83eba
Lauren Stone <lstone321@gmail.com>
2020-07-19 01:02:48 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,10 +30,14 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 shipping_manifest["muskets"] 
-  shipping_manifest["muskets"] = 2 
   # add 4 gun powder to the shipping_manifest hash below
 shipping_manifest["gun powder"] = 4
 
   # return the shipping_manifest hash below
-  puts shipping_manifest
+  puts shipping_manifest = "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3
+                  "muskets" => 2 
+                  "gun powder" => 4
+                 }
 end
